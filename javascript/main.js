65160df6aa8109704f501717a1abcab4cd2b8c07
SadCoder-01 <fridaydamilare22@gmail.com>
2021-05-19 22:57:17 +0100

    still working on

---
+++ javascript/main.js
@@ -0,0 +1,14 @@
+// const currentSlide = 0;
+// const slide = document.querySelectorAll(".fade");
+
+// function nextSlide(n){
+//     slide.forEach((slide) =>{
+//         slide.style.display = "none";
+//     })
+// };
+
+// slide[n].style.display = "block";
+
+// document.addEventListener("DOMContentLoaded", init(currentSlide));
+
+// document.querySelector(".left").addEventListener('click', next);
