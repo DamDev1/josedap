ebbfef9a2e26bbbe9977efe6997e731f224e8d5a
SadCoder-01 <fridaydamilare22@gmail.com>
2021-05-24 00:38:07 +0100

    first commit

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,22 @@
+const toggleOpen =document.getElementById("toggleOpen");
+const toggleClose = document.getElementById("toggleClose");
+const navbar = document.getElementById("navbar");
+
+navbar.style.display = "none";
+
+toggleOpen.addEventListener("click", () =>{
+    if(navbar.style.display == "none"){
+        navbar.style.display = "block";
+    }
+})
+
+toggleClose.addEventListener("click", () =>{
+    if(navbar.style.display == "block"){
+        navbar.style.display = "none";
+    }
+});
+
+
 // const currentSlide = 0;
 // const slide = document.querySelectorAll(".fade");
 
